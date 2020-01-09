9a1e470d3ca48eea062de2340d26253a1edc6419
Alchemy <Alchemy@192.168.1.67>
2020-01-09 11:14:20 +0530

    finally

---
+++ Finally/src/finallyworks/MyException.java
@@ -0,0 +1,18 @@
+package finallyworks;
+
+public class MyException extends Exception {
+
+	private String message;
+
+	public MyException(String message) {
+		super();
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return  message;
+	}
+
+}
+
