cd77ec69adbf9db7f6024613c30348b144621a79
Alchemy <Alchemy@192.168.1.67>
2020-01-09 16:50:18 +0530

    mylist

---
+++ ToDo1/src/comm/example/ToDoDaoImpl.java
@@ -0,0 +1,112 @@
+package comm.example;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+
+
+public class ToDoDaoImpl implements ToDoDao {
+	
+	private List<ToDo> toDoList=null;
+	private ToDo toDo;
+	{
+		toDoList=new ArrayList<ToDo>();
+	}
+
+	@Override
+	public ToDo createToDo(String toDoName) {
+		String str[]=UUID.randomUUID().toString().split("-");
+		toDo=new ToDo();
+		toDoList.add(toDo);
+		return toDo;
+		
+	}
+
+	@Override
+	public List<ToDo> getAllToDos() {
+		// TODO Auto-generated method stub
+		if(toDoList.isEmpty())
+		{
+			System.out.println("list is empty");
+		}
+		return toDoList;
+	}
+
+	@Override
+	public ToDo findById(String toDoId) {
+		// TODO Auto-generated method stub
+	
+			Iterator<ToDo> iterator=toDoList.iterator();
+			
+			while(iterator.hasNext())
+			{
+				toDo=iterator.next();
+				if(toDo.getToDoId()==(toDoId))
+				{
+					
+					System.out.println(toDo.getToDoId());
+					break;
+				}
+			}
+		
+		
+		return toDo;
+	}
+
+	@Override
+	public void deleteToDo(String toDoId) {
+		if(toDoList.isEmpty())
+		{
+			System.out.println("todo list is empty.");
+			
+		}
+		toDo=findById(toDoId);
+		if(toDo==null)
+		{
+			System.out.println("no such todo to delete");
+		}
+		else
+		{
+			toDoList.remove(toDo);
+			System.out.println("remoded sucessfully with id: "+toDoId);
+		}
+		
+	}
+
+	@Override
+	public void removeAllToDo() {
+		toDoList.removeAll(toDoList);
+		
+	}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ ToDo1/src/comm/example/ToDoService.java
@@ -0,0 +1,14 @@
+package comm.example;
+
+import java.util.List;
+
+public interface ToDoService {
+
+	public ToDo createToDo(String toDoName);
+	public List<ToDo> getAllToDos();
+	public ToDo findById(String toDoId);
+
+	public void deleteToDo(String toDoId);
+	public void removeAllToDo();
+}
+
