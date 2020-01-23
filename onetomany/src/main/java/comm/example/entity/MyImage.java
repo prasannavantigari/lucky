89845a91fc1897677bffcd0dbb9fe80bad101412
Alchemy <Alchemy@192.168.1.67>
2020-01-23 12:59:01 +0530

    one to many

---
+++ onetomany/src/main/java/comm/example/entity/MyImage.java
@@ -0,0 +1,39 @@
+package comm.example.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="album_detail")
+public class MyImage {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="image_url")
+	private String imageUrl;
+	
+
+		@OneToOne
+	private Album album;
+	
+	public MyImage(String imageUrl) {
+		super();
+		this.imageUrl=imageUrl;
+
+	
+}
+}
