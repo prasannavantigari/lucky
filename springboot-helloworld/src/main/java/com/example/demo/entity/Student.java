b62f0c4d0ad59248f0b3e9ac8a7491ce4efa635d
Alchemy <Alchemy@192.168.1.67>
2020-02-03 17:33:17 +0530

    springboot-helloworld

---
+++ springboot-helloworld/src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String uId;
+	private String firstName;
+	private String lastName;
+	private String email;
+}
---
+++ springboot-helloworld/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student(first_name,last_name,email) values('John','Doe','john@email.com');
+insert into student(first_name,last_name,email) values('suppi','suppu','suppi@email.com');
+insert into student(first_name,last_name,email) values('navii','navaa','navii@email.com');
+insert into student(first_name,last_name,email) values('sravs','sravsee','sravs@email.com');
